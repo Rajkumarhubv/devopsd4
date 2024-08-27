1ae1b11ff236ea9287b561b5c20e99cdcb709a78
AdmiN <AdmiN@ITWS000>
2024-08-27 10:19:06 +0530

    commit

---
+++ devopsd4/src/devopsd4/devopsd4proj.java
@@ -0,0 +1,10 @@
+package devopsd4;
+
+public class devopsd4proj {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("helloodjfjfkljjkdf");
+	}
+
+}
